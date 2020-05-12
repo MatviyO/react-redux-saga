2b7e20a2a086de3fa2b96d487f38fc257c3b5921
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-12 21:04:03 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostForm from "./component/PostForm";
+import Posts from "./component/Posts";
+import FetchedPost from "./component/FetchedPost";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container pt-3">
+            <div className="row">
+                <div className="col">
+                    <PostForm />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h2>Sync Posts</h2>
+                    <Posts posts={[1,2,3]} />
+                </div>
+                <div className="col">
+                    <h2>Asycn posts</h2>
+                    <FetchedPost posts={[1,2,3]} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/Posts.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Post from "./Post";
+export default ({posts}) => {
+    return posts.map(post => <Post post={post} id={post} />)
+}
